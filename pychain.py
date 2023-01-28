e44a0ceed2885eee9b237fbe11edcf44365a04f7
Ven B <venkata.nagothi@gmail.com>
2023-01-28 15:39:20 +1100

    Update pychain.py - transactions working..

--- pychain.py
+++ pychain.py
@@ -51,6 +51,11 @@ import hashlib
 # `amount` attributes
 # YOUR CODE HERE
 
+@dataclass
+class Record:
+   sender: str
+   receiver: str
+   amount: float 
 
 ################################################################################
 # Step 2:
@@ -68,7 +73,7 @@ class Block:
 
     # @TODO
     # Rename the `data` attribute to `record`, and set the data type to `Record`
-    data: Any
+    record: Record
 
     creator_id: int
     prev_hash: str = "0"
@@ -166,19 +171,18 @@ pychain = setup()
 
 # @TODO:
 # Delete the `input_data` variable from the Streamlit interface.
-input_data = st.text_input("Block Data")
 
 # @TODO:
 # Add an input area where you can get a value for `sender` from the user.
-# YOUR CODE HERE
+sender = st.text_input("Sender Details")
 
 # @TODO:
 # Add an input area where you can get a value for `receiver` from the user.
-# YOUR CODE HERE
+receiver = st.text_input("Receiver Details")
 
 # @TODO:
 # Add an input area where you can get a value for `amount` from the user.
-# YOUR CODE HERE
+amount = st.text_input("Enter the Amount to be transferred")
 
 if st.button("Add Block"):
     prev_block = pychain.chain[-1]
@@ -189,7 +193,7 @@ if st.button("Add Block"):
     # which is set equal to a `Record` that contains the `sender`, `receiver`,
     # and `amount` values
     new_block = Block(
-        data=input_data,
+        record=Record(sender,receiver,amount),
         creator_id=42,
         prev_hash=prev_block_hash
     )
